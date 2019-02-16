7354c273e904f8460947682b06c1fd94356e98d2
surasnayak <surasnayak@gmail.com>
2019-02-16 15:58:12 +0530

    added contributors section

--- src/Navigation.js
+++ src/Navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => {
 		 	<ul className="list mt0 mb0 pt3 pb3 bg-light-red white fw6 flex o-70 ttu tracked">
 		 		<a className="link white" href="#"><li className="ml3">HOME</li></a>
 		 		<a className="link white" href="#gallery"><li className="ml3">Gallery</li></a>
-		 		<a className="link white" href="#"><li className="ml3">Contributors</li></a>
+		 		<a className="link white" href="#contributors"><li className="ml3">Contributors</li></a>
 		 		<a className="link white ml-auto" href="#"><li className="ml-auto mr4">Contact</li></a>
 		 	</ul>
 		</nav>
