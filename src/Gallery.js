5992cbabd0b18bee4db8674ef9f75879a6b8b38b
surasnayak <surasnayak@gmail.com>
2019-02-18 22:15:08 +0530

    added search bar to gallery section

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from './Card';
 import { images } from './images';
+import './Gallery.css'
 
-const Gallery = () => {
-
-	const imageCards = []; 
-	for(let i=0; i<images.length; i++)
-	{
-		imageCards.push(<Card imageContent={images[i]} />);
+class Gallery extends Component {
+	constructor() {
+		super();
+		this.state = {
+			stateImages: images,
+			searchField: ''
+		}
 	}
 
-	return(
-		<div id="gallery">
-			<h3 className="card-styles f1 tracked tc light-red o-90 pa5">GALLERY</h3>
-			{imageCards}
-		</div>
-	);
+	onSearch = (event) => {
+		this.setState({ searchField: event.target.value });
+	}
+
+	render() {
+		const imageCards = []; 
+		for(let i=0; i<this.state.stateImages.length; i++)
+		{
+			if(this.state.stateImages[i].title.toLowerCase().includes(this.state.searchField.toLowerCase()) || 
+				this.state.stateImages[i].author.toLowerCase().includes(this.state.searchField.toLowerCase()))
+			{
+				imageCards.push(<Card imageContent={this.state.stateImages[i]} />);
+			}
+		}
+		
+		return(
+			<div id="gallery">
+				<h3 className="gallery-heading f1 tracked tc light-red o-90 pa5">GALLERY</h3>
+				<div className="search-box tc mt3">
+					<input 
+						className="pa2 br3 b--light-red shadow-2 red" 
+						type="search" 
+						placeholder="search in galllery"
+						onChange={this.onSearch} 
+					/>
+				</div>
+				{imageCards}
+			</div>
+		);
+	}
 }
 
 export default Gallery;
